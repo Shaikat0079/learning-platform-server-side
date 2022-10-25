2399abc5b728be170c2c6dd3603c18f65eafc8f0
Shaikat0079 <baruashaikat00790@gmail.com>
2022-10-25 17:09:11 +0600

    Cors Working

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const port = process.env.PORT || 5002;
+
+app.use(cors())
 
 const courses = require('./data/courses.json')
 
